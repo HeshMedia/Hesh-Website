9d2cfd7b2e2a73f4363cbb31b1f767efc43f728c
HeshMedia <heshmedia21@gmail.com>
2024-12-26 01:11:32 +0530

    changed smm assets to default

--- src/pages/smmPage.tsx
+++ src/pages/smmPage.tsx
@@ -2,9 +2,9 @@ import { ServiceLayout } from "@/components/serviceLayout";
 
 export function SMMPage() {
   const workExamples = [
-    "/assets/office.jpg",
-    "assets/smm_2.jpg",
-    "assets/smm_3.jpg",
+    "/assets/k.jpg",
+    "assets/k.jpg",
+    "assets/k.jpg",
   ];
 
   const smmContent = `
@@ -19,7 +19,7 @@ export function SMMPage() {
       title="Social Media Management"
       description="Transform your social presence with professional, consistent branding and engaging posts."
       content={smmContent}
-      images={["/assets/office.jpg", "/assets/office2.jpg"]}
+      images={["/assets/k.jpg", "/assets/k.jpg"]}
     >
       {workExamples.map((image, index) => (
         <img
